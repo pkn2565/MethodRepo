6cc6e69fef86b0ae75e3a50168d7762eed5ef89c
pkn2565 <pkn2565@email.vccs.edu>
2017-11-26 17:46:47 -0500

    Initial commit

---
+++ src/MoreMath.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class MoreMath {
+    public static void main(String[] args){
+        int N,F,S,q,num=e,i;
+        N-Integer.parseInt(JOptionPane.showInputDialog("Enter an Int Number"));
+        F=1;
+        while(N > 0){
+            F *= N;
+            N--;
+
+        }
+    }JOptionPane
+}
